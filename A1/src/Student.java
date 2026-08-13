18c42a89804b9f20ab733137efbd56d99d526a46
Minh Hoang Pham <phamminhhoang317@gmail.com>
2026-08-13 15:21:02 +1000

    A1: Clean up data type and constructor

--- A1/src/Student.java
+++ A1/src/Student.java
@@ -1,15 +1,12 @@
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Student {
     private String name;
-    private Set<String> enrolledCourses;
+    private Set<String> enrolledCourses = new HashSet<>();
 
-    public Student(
-        String name,
-        Set<String> enrolledCourses
-    ) {
+    public Student(String name) {
         this.name = name;
-        this.enrolledCourses = enrolledCourses;
     }
 }
